939452b63ca2924940d2d4256ebcf76f491e21e7
trev191 <trev191@yahoo.com>
2024-03-02 01:01:31 -0800

    Create basic search bar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import Title from './components/Title'
+import Title from './components/Title';
+import SearchBar from './components/SearchBar';
+
+const titleStr = "Forum";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <Title title="Forum"/>
-        <div className="SearchBar">
-          Search
-        </div>
+        <Title title={titleStr}/>
+        <SearchBar />
       </div>
     </div>
   );
